914fc0d802cea92efc5d3c220ea42332534e3d0d
teddy <teddywambua5456@gmail.com>
2023-06-26 00:23:31 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :birds, only: [:index, :show]
+  get '/birds', to: 'birds#index'
+  get '/birds/:id', to: 'birds#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
